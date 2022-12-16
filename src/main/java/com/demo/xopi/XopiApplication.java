f877206651ad3dcf8207783335858796167a3012
Jonathan Henrique <jonathan.mdr@hotmail.com>
2022-12-15 21:36:42 -0300

    Skeleton

---
+++ src/main/java/com/demo/xopi/XopiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.xopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XopiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XopiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/xopi/XopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.xopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XopiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
